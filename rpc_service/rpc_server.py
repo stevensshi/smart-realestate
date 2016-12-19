08cb4f991986db3797539254a154f29bbf953f19
stevensshi <steven_s_shi@hotmail.com>
2016-12-19 20:07:31 +1030

    remove page files

--- rpc_service/rpc_server.py
+++ rpc_service/rpc_server.py
@@ -2,29 +2,30 @@ import pyjsonrpc
 import os
 import sys
 import json
+import re
 
 from bson.json_util import dumps
 
 # import mongodb path
-sys.path.append(os.path.join.dirname(__file__), '..', 'mongodb')
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'mongodb'))
 
 SERVER_HOST = 'localhost'
 SERVER_PORT = 4040
 
-PROTERTY_TABLE_NAME = 'properties'
+PROTERTY_TABLE_NAME = 'property'
 
 class RequestHandler(pyjsonrpc.HttpRequestHandler):
-    # """Test Method"""
+    """Test Method"""
     @pyjsonrpc.rpcmethod
     def add(self, a, b):
         print "add gets called with %d and %d" % (a, b)
         return a+b
 
+
     @pyjsonrpc.rpcmethod
     def searchArea(self, query):
         res = []
         if query.isdigit():
-            # TODO: search in DB
             print "postcode"
             db = mongodb_client.getDB()
             res = db[PROPERTY_TABLE_NAME].find({'postcode':query})
@@ -32,7 +33,9 @@ class RequestHandler(pyjsonrpc.HttpRequestHandler):
         else:
             city = query.split(',')[0].strip()
             state = query.split(',')[1].strip()
-            # TODO: search in DB
+            res = db[PROPERTY_TABLE_NAME].find({'city': re.compile(city, re.IGNORECASE),
+                                                'state': re.compile(state, re.IGNORECASE)})
+            res = json.loads(dumps(res))
         return res
 
 
