f7e53b884a2e3d96d8326b1c902260ac166fc1f7
stevensshi <steven_s_shi@hotmail.com>
2016-12-28 08:28:40 +1030

    zillow web scraper

--- mongodb/mongodb_client.py
+++ mongodb/mongodb_client.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 MONGO_DB_HOST = 'localhost'
 MONGO_DB_PORT = '27017'
-DB_NAME = 'smart_realestate_view'
+DB_NAME = 'test'
 
 client = MongoClient('%s:%s'% (MONGO_DB_HOST, MONGO_DB_PORT))
 
